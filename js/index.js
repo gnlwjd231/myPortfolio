2b35fdefa3a976837ebe037c50b68e41277e43e8
Hwi Jeong Park <heee2x1@gmail.com>
2021-10-26 17:59:20 +0900

    20211026
    css 반응형으로 다시 짜기

--- js/index.js
+++ js/index.js
@@ -1,137 +1,137 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var vailForStartAnmiate = $('#vailForStartAnmiate');
+// document.addEventListener("DOMContentLoaded", function(){
+//     var vailForStartAnmiate = $('#vailForStartAnmiate');
 
-    var cardNameTagContainer = $('#nametagImgContainer');
-    var keywordContainer = $('#keywordContainer');
-    var titleContainer = $('#titleContainer');
-    var menuContainer = $('#menuContainer');
-    var menu = $('#menuContainer > ul > li');
-    var keywords = $('#keywordContainer > div.keyword');
+//     var cardNameTagContainer = $('#nametagImgContainer');
+//     var keywordContainer = $('#keywordContainer');
+//     var titleContainer = $('#titleContainer');
+//     var menuContainer = $('#menuContainer');
+//     var menu = $('#menuContainer > ul > li');
+//     var keywords = $('#keywordContainer > div.keyword');
 
-    console.log(vailForStartAnmiate);
+//     console.log(vailForStartAnmiate);
 
-    vailForStartAnmiate.on("click", function(){
+//     vailForStartAnmiate.on("click", function(){
         
-        vailForStartAnmiate.remove();
+//         vailForStartAnmiate.remove();
 
-        if($('body').width() > 767){
+//         if($('body').width() > 767){
 
-            titleContainer.animate({
-                top: "-=100",
-                opacity: "1"
-            }, 500, function() {
-                $(this).css('opacity','1');
-                // $(this).css('top','626.75px');
-            });
+//             titleContainer.animate({
+//                 top: "-=100",
+//                 opacity: "1"
+//             }, 500, function() {
+//                 $(this).css('opacity','1');
+//                 $(this).css('top','626.75px');
+//             });
     
-            cardNameTagContainer.animate({
-                top: "-=100",
-            }, 500, function() {
-                // $(this).css('top','-100px');
-            });
+//             cardNameTagContainer.animate({
+//                 top: "-=100",
+//             }, 500, function() {
+//                 $(this).css('top','-100px');
+//             });
     
-            menuContainer.animate({
-                opacity: "1"
-            }, 500, function() {
-                $(this).css('opacity','1');
-            });
+//             menuContainer.animate({
+//                 opacity: "1"
+//             }, 500, function() {
+//                 $(this).css('opacity','1');
+//             });
     
-            keywordContainer.animate({
-                opacity: "1"
-            }, 500, function() {
-                $(this).css('opacity','1');
-            });
+//             keywordContainer.animate({
+//                 opacity: "1"
+//             }, 500, function() {
+//                 $(this).css('opacity','1');
+//             });
 
-        }else{
+//         }else{
 
-            cardNameTagContainer.animate({
-                top: "-=10%",
-            }, 500, function() {
-                // $(this).css('top','-100px');
-            });
+//             cardNameTagContainer.animate({
+//                 top: "-=10%",
+//             }, 500, function() {
+//                 $(this).css('top','-100px');
+//             });
 
-            keywordContainer.animate({
-                opacity: "1"
-            }, 500, function() {
-                $(this).css('opacity','1');
-            });
-        }
+//             keywordContainer.animate({
+//                 opacity: "1"
+//             }, 500, function() {
+//                 $(this).css('opacity','1');
+//             });
+//         }
         
         
 
-    });
+//     });
 
     
 
-    menu.on("click", function(){
-        let deActiveTarget =  $('#menuContainer > ul > li.active');
+//     menu.on("click", function(){
+//         let deActiveTarget =  $('#menuContainer > ul > li.active');
         
-        deActiveTarget.removeClass('active');
-        $(this).addClass('active');
+//         deActiveTarget.removeClass('active');
+//         $(this).addClass('active');
 
-        if( menu.index($(this)) == 0 && cardNameTagContainer.css('opacity')==0
-            || menu.index($(this)) == 3 && cardNameTagContainer.css('opacity')==0 ){
+//         if( menu.index($(this)) == 0 && cardNameTagContainer.css('opacity')==0
+//             || menu.index($(this)) == 3 && cardNameTagContainer.css('opacity')==0 ){
 
-            titleContainer.animate({
-                top: "+=100",
-                opacity: "1"
-            }, 500, function() {
-            });
+//             titleContainer.animate({
+//                 top: "+=100",
+//                 opacity: "1"
+//             }, 500, function() {
+//             });
 
-            cardNameTagContainer.animate({
-                top: "+=100",
-                opacity: "1"
-            }, 500, function() {
-            });
+//             cardNameTagContainer.animate({
+//                 top: "+=100",
+//                 opacity: "1"
+//             }, 500, function() {
+//             });
 
-            keywordContainer.animate({
-                top: "+=400px",
-                opacity: "1"
-            }, 500, function() {
-            });
+//             keywordContainer.animate({
+//                 top: "+=400px",
+//                 opacity: "1"
+//             }, 500, function() {
+//             });
 
-            menuContainer.animate({
-                top: "+=780px",
-            }, 500, function() {
-            });
+//             menuContainer.animate({
+//                 top: "+=780px",
+//             }, 500, function() {
+//             });
 
-        }else if( menu.index($(this)) == 1 && cardNameTagContainer.css('opacity')==1 
-            || menu.index($(this)) == 2 && cardNameTagContainer.css('opacity')==1 ){
+//         }else if( menu.index($(this)) == 1 && cardNameTagContainer.css('opacity')==1 
+//             || menu.index($(this)) == 2 && cardNameTagContainer.css('opacity')==1 ){
             
-            titleContainer.animate({
-                top: "-=100px",
-                opacity: "0"
-            }, 500, function() {
-                $(this).css('opacity','0');
-                // $(this).css('top','526.75px');
-            });
+//             titleContainer.animate({
+//                 top: "-=100px",
+//                 opacity: "0"
+//             }, 500, function() {
+//                 $(this).css('opacity','0');
+//                 $(this).css('top','526.75px');
+//             });
 
-            cardNameTagContainer.animate({
-                top: "-=100px",
-                opacity: "0"
-            }, 500, function() {
-                $(this).css('opacity','0');
-                // $(this).css('top','-200px');
-            });
+//             cardNameTagContainer.animate({
+//                 top: "-=100px",
+//                 opacity: "0"
+//             }, 500, function() {
+//                 $(this).css('opacity','0');
+//                 $(this).css('top','-200px');
+//             });
 
-            keywordContainer.animate({
-                top: "-=100px",
-                opacity: "0"
-            }, 500, function() {
-                $(this).css('opacity','0');
-                // $(this).css('top','-300px');
-            });
+//             keywordContainer.animate({
+//                 top: "-=100px",
+//                 opacity: "0"
+//             }, 500, function() {
+//                 $(this).css('opacity','0');
+//                 $(this).css('top','-300px');
+//             });
 
-            menuContainer.animate({
-                top: "-=780px",
-            }, 1100, function() {
-                // $(this).css('top', '60.109px');
-            });
+//             menuContainer.animate({
+//                 top: "-=780px",
+//             }, 1100, function() {
+//                 $(this).css('top', '60.109px');
+//             });
 
-        }else{
-            console.log($(this));
-        }
+//         }else{
+//             console.log($(this));
+//         }
         
-    });
+//     });
 
-});
+// });
